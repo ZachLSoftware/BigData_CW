533b98d7afcfa894238c365d130709bd63275800
Zach Larsen <zacharyjlarsen@gmail.com>
2023-03-08 20:40:41 +0000

    scatter matrix

--- main_zach.py
+++ main_zach.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import seaborn as sns
+from pandas.plotting import scatter_matrix
 
 
 def markOutliers(df,columns):
@@ -11,7 +12,7 @@ def markOutliers(df,columns):
     for column in columns:
         mean=np.mean(df[column])
         sd=np.std(df[column])
-        threshold=3
+        threshold=2
         test=[]
 
         for i, x in enumerate(df[column]):
@@ -49,14 +50,13 @@ def step1_clean():
     categorical=['BOROUGH','NEIGHBORHOOD', 'BUILDING CLASS CATEGORY', 'TAX CLASS AT PRESENT', 'BLOCK','LOT','EASE-MENT', 'BUILDING CLASS AT PRESENT', 'ADDRESS', 'APARTMENT NUMBER','ZIP CODE','YEAR BUILT','TAX CLASS AT TIME OF SALE', 'BUILDING CLASS AT TIME OF SALE', 'SALE DATE'
     ]
     # df_num_man=df.filter(['RESIDENTIAL UNITS', 'COMMERCIAL UNITS', 'TOTAL UNITS', 'LAND SQUARE FEET', 'GROSS SQUARE FEET', 'SALE PRICE']).copy()
-    num_cols=df[numerical].copy()
-    cat_cols=df[categorical].copy()
+
     #num_cols=pd.concat([num_cols,df_num_man], axis=1, join='inner')
     df[numerical]=df[numerical].replace('\$','', regex=True)
     df[numerical]=df[numerical].replace(',','', regex=True)
 
 
-    cat_cols['SALE DATE']=pd.to_datetime(cat_cols['SALE DATE'], dayfirst=True)
+    df['SALE DATE']=pd.to_datetime(df['SALE DATE'], dayfirst=True)
     df[categorical]=df[categorical].replace(' ', '', regex=True)
     df[categorical]=df[categorical].replace('', np.NaN)
 
@@ -76,24 +76,52 @@ def step1_clean():
     return df
 
 
-def normalize(df):
+def normalize(df,num_cols):
     #select numerical columns
     df_norm=df.copy()
-    num_cols = df.select_dtypes(include=[np.number]).copy()
     # num_cols.drop('SALE PRICE', axis='columns', inplace=True)
-    df_norm[num_cols.columns]=df_norm[num_cols.columns]/df_norm[num_cols.columns].abs().max()#((num_cols-num_cols.min())/(num_cols.max()-num_cols.min()))
+    df_norm[num_cols]=df_norm[num_cols]/df_norm[num_cols].abs().max()#((num_cols-num_cols.min())/(num_cols.max()-num_cols.min()))
     return df_norm
 
 df = step1_clean()
 df.reset_index(drop=True,inplace=True)
-print(df.head())
+numerical=['RESIDENTIAL UNITS','COMMERCIAL UNITS','TOTAL UNITS','LAND SQUARE FEET','GROSS SQUARE FEET','SALE PRICE']
+categorical=['NEIGHBORHOOD', 'BUILDING CLASS CATEGORY', 'TAX CLASS AT PRESENT', 'BLOCK','LOT', 'BUILDING CLASS AT PRESENT', 'ADDRESS', 'ZIP CODE','YEAR BUILT','TAX CLASS AT TIME OF SALE', 'BUILDING CLASS AT TIME OF SALE', 'SALE DATE']
+
 
 df['lnprice']=np.log(df['SALE PRICE'])
 # df.to_csv('./tempnonenorm.csv',index=True)
-dfnorm=normalize(df)
+dfnorm=normalize(df, numerical)
 
-numerical=['RESIDENTIAL UNITS','COMMERCIAL UNITS','TOTAL UNITS','LAND SQUARE FEET','GROSS SQUARE FEET','SALE PRICE']
-categorical=['NEIGHBORHOOD', 'BUILDING CLASS CATEGORY', 'TAX CLASS AT PRESENT', 'BLOCK','LOT', 'BUILDING CLASS AT PRESENT', 'ADDRESS', 'ZIP CODE','YEAR BUILT','TAX CLASS AT TIME OF SALE', 'BUILDING CLASS AT TIME OF SALE', 'SALE DATE']
+
+
+# plt.figure(figsize=[20,10])
+# sns.boxplot( y=dfnorm["SALE PRICE"], x=dfnorm["NEIGHBORHOOD"] )
+# plt.xticks(rotation=90)
+# plt.show()
+
+# fig, axes=plt.subplots(figsize=[20,10])
+# sns.lineplot(y="SALE PRICE", x="SALE DATE", data=dfnorm)
+# plt.xticks(rotation = 'vertical')
+# plt.show()
+
+# fig, axes=plt.subplots(figsize=[20,10])
+# sns.lineplot(y="SALE PRICE", x="YEAR BUILT", data=dfnorm)
+# plt.xticks(rotation = 'vertical')
+# plt.show()
+
+fig = plt.figure(1, figsize=[12, 12])
+fig.clf()
+ax = fig.gca()
+matrix=scatter_matrix(dfnorm, alpha=0.3, diagonal='kde', ax = ax)
+for ax in matrix.flatten():
+    ax.xaxis.label.set_rotation(90)
+    ax.yaxis.label.set_rotation(0)
+    ax.yaxis.label.set_ha('right')
+
+plt.tight_layout()
+plt.gcf().subplots_adjust(wspace=0, hspace=0)
+plt.show()
 
 # #for column in numerical:
 # f, ax = plt.subplots()
@@ -104,14 +132,14 @@ categorical=['NEIGHBORHOOD', 'BUILDING CLASS CATEGORY', 'TAX CLASS AT PRESENT', 
 # plt.show()
 # # df.to_csv('./temp.csv',index=True)
 ## Create Boxplots of data
-def auto_boxplot(df, plot_cols, by):
-    import matplotlib.pyplot as plt
-    for col in plot_cols:
-        fig = plt.figure(figsize=(9, 6))
-        ax = fig.gca()
-        df.boxplot(column = col, by = by, ax = ax)
-        ax.set_title('Box plots of {} bg {}'.format(col, by))
-        ax.set_ylabel(col)
-        plt.show()
+# def auto_boxplot(df, plot_cols, by):
+#     import matplotlib.pyplot as plt
+#     for col in plot_cols:
+#         fig = plt.figure(figsize=(9, 6))
+#         ax = fig.gca()
+#         df.boxplot(column = col, by = by, ax = ax)
+#         ax.set_title('Box plots of {} bg {}'.format(col, by))
+#         ax.set_ylabel(col)
+#         plt.show()
 
-auto_boxplot(df,numerical, 'NEIGHBORHOOD')
+# auto_boxplot(df,numerical, 'NEIGHBORHOOD')
