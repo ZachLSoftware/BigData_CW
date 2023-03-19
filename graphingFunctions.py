58e55fb2e8b091c59dba79341b2b56b09864d3d1
Zach Larsen <zacharyjlarsen@gmail.com>
2023-03-19 23:24:06 +0000

    Getting pretty good scores...

---
+++ graphingFunctions.py
@@ -0,0 +1,66 @@
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas.plotting import scatter_matrix
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+
+def boxPlot(df, y, x):
+    plt.figure(figsize=[20,10])
+    sns.boxplot( y=df[y], x=df[x] )
+    plt.xticks(rotation=90)
+    plt.show()
+
+def averagePrice(df, y, x):
+    plt.figure(figsize=[20,10])
+    data=df.groupby(x)[y].mean().reset_index()
+    sns.barplot(x=x,y=y, data=data)
+    plt.xticks(rotation=90)
+    plt.show()
+
+def priceSaleDate(df):
+    fig, axes=plt.subplots(figsize=[20,10])
+    sns.lineplot(y="lnprice", x="SALE_DATE", data=df)
+    plt.xticks(rotation = 'vertical')
+    plt.show()
+
+
+def priceOverYearBuilt(df):
+    fig, axes=plt.subplots(figsize=[20,10])
+    sns.lineplot(y="lnprice", x="YEAR_BUILT", data=df)
+    plt.xticks(rotation = 'vertical')
+    plt.show()
+
+def scatterMatrix(df):
+    fig = plt.figure(1, figsize=[20, 20])
+    fig.clf()
+    ax = fig.gca()
+    matrix=scatter_matrix(df, alpha=0.3, diagonal='kde', ax = ax)
+    for ax in matrix.flatten():
+        ax.xaxis.label.set_rotation(90)
+        ax.yaxis.label.set_rotation(0)
+        ax.yaxis.label.set_ha('right')
+
+    plt.tight_layout()
+    plt.gcf().subplots_adjust(wspace=0, hspace=0)
+    plt.show()
+
+
+def heatMap(df):
+    plt.figure(figsize=(16, 6))
+    heatmap = sns.heatmap(df.corr(), vmin=-1, vmax=1, annot=True)
+    heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12)
+    plt.show()
+
+def regressionFitNumerical(df, column):
+    f='lnprice~'+column
+    model = smf.ols(formula=f, data=df).fit()
+    predicted=model.predict(df[column])
+    plt.plot(df[column],df["lnprice"], 'bo')
+    plt.plot(df[column], predicted, 'r-', linewidth=2)
+    #plt.title('Linear Regression Fit')
+    plt.show()
+    print('\n',model.params)
+    print("confidence interval:\n", model.conf_int(alpha=0.05),'\n')
+    print("P values:\n", model.pvalues)
+    print("R-Squared", model.rsquared)
---
+++ modelingFunctions.py
@@ -0,0 +1,60 @@
+import numpy as np
+from sklearn import svm, feature_selection, linear_model
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+import seaborn as sns
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+
+def linearRegressionModel(df, target, select_features):
+    # Select predictors
+    X = df[select_features]
+
+    # Encode categorical variables using one-hot encoding
+    #X = pd.get_dummies(X)
+
+    # Target variable
+    y = df['lnprice']
+
+    # Split the data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Fit the model
+    model = linear_model.LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Evaluate the model on the test set
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    print("Root Mean Squared Error: {:.2f}".format(rmse))
+
+    # Evaluate the model using cross-validation
+    cv_scores = cross_val_score(model, X, y, cv=5)
+    print("Cross-validation scores: ", cv_scores)
+    print("Mean cross-validation score: {:.2f}".format(np.mean(cv_scores)))
+
+    print("Y-axis intercept {:6.4f}".format(model.intercept_))
+    print("Weight coefficients:")
+    for feat, coef in zip(select_features, model.coef_):
+        print(" {:>20}: {:6.4f}".format(feat, coef))
+    # The value of R^2
+    print("R squared for the training data is {:4.3f}".format(model.score(X_train,
+    y_train)))
+    print("Score against test data: {:4.3f}".format(model.score(X_test, y_test)))
+
+    # Plot histogram of residuals
+    residuals = y_test - y_pred
+    sns.histplot(residuals, kde=True)
+    return model
+
+def itImputations(df, columns):
+    imputer = IterativeImputer(max_iter=50, random_state=42)
+    print("RELOADED")
+    # define the columns we want to impute
+    cols_to_impute = columns
+
+    # fit and transform the imputer on the columns we want to impute
+    df[cols_to_impute] = imputer.fit_transform(df[cols_to_impute])
+    return df
